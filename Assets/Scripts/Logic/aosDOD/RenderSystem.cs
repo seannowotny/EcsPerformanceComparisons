8df36264b085931068c19a6df41a007bbf5a8c1d
seannowotny <seannowotny@live.de>
2023-06-03 17:07:02 +0200

    add array-of-structs DOD implementation

---
+++ Assets/Scripts/Logic/aosDOD/RenderSystem.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Sean Nowotny
+
+using UnityEngine;
+
+namespace Logic.aosDOD
+{
+    public static class RenderSystem
+    {
+        private static Transform[] transformPool = new Transform[Data.MaxVehicleCount];
+        private static MeshRenderer[] meshPool = new MeshRenderer[Data.MaxVehicleCount];
+
+        public static void Run(GameObject prefab, Material[] materials)
+        {
+            if (meshPool[0] == null)
+            {
+                for (var i = 0; i < transformPool.Length; i++)
+                {
+                    transformPool[i] = GameObject.Instantiate(prefab).transform;
+                    meshPool[i] = transformPool[i].GetComponent<MeshRenderer>();
+                }
+            }
+
+            for (var i = 0; i < Data.Vehicles.Length; i++)
+            {
+                ref var vehicle = ref Data.Vehicles[i];
+                if (!vehicle.IsAlive)
+                {
+                    meshPool[i].enabled = false;
+                    continue;
+                }
+
+                var position = vehicle.Position;
+                transformPool[i].position = new Vector3(position.x, 0, position.y);
+                meshPool[i].enabled = true;
+                meshPool[i].material = materials[vehicle.Team];
+            }
+        }
+    }
+}
