9a22ad1a68127d211f97490c1970c7daff3f5a35
seannowotny <seannowotny@live.de>
2023-06-04 11:05:45 +0200

    improve Mathematics.Random usage

--- Assets/Scripts/Logic/BurstedAosDOD/StaticGameHandler.cs
+++ Assets/Scripts/Logic/BurstedAosDOD/StaticGameHandler.cs
@@ -10,11 +10,13 @@ namespace Logic.BurstedAosDOD
         [BurstCompile]
         public static void BurstedUpdate(float deltaTime, ref Data data)
         {
+            Unity.Mathematics.Random random = new Unity.Mathematics.Random((uint) (deltaTime * 10000000));
+            
             EnemyTargetSystem.Run(ref data);
             VehicleMovementSystem.Run(deltaTime, ref data);
             ShootSystem.Run(deltaTime, ref data);
             
-            SpawnVehiclesSystem.Run(deltaTime, ref data);
+            SpawnVehiclesSystem.Run(ref random, ref data);
             DieSystem.Run(ref data);
         }
     }
